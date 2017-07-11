28d9f79dfdc9924ca7d704353df576012e841bcb
Cédric Dupont <lunarok@gmail.com>
2017-07-11 15:24:16 +0200

    avoid issue when no internet connexion with curl

--- core/class/telegram.class.php
+++ core/class/telegram.class.php
@@ -132,6 +132,8 @@ class telegramCmd extends cmd {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        //curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         $output = curl_exec($ch);
     }
 
