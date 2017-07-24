6a0b7721d8df99f721a338efb243cfdb509bf977
Cédric Dupont <lunarok@gmail.com>
2017-07-24 10:52:23 +0200

    answer 200 to webhook API and avoid resend

--- core/api/jeeTelegram.php
+++ core/api/jeeTelegram.php
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
+http_response_code(200);
 header("Content-Type: application/json");
 require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
 
