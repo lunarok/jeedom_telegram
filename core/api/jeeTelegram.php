dc489b7acf8fdabf747ef8f35af950987bcc0f73
Cédric Dupont <lunarok@gmail.com>
2017-08-27 16:24:32 +0200

    bugfix

--- core/api/jeeTelegram.php
+++ core/api/jeeTelegram.php
@@ -120,7 +120,7 @@ if (isset($json["message"]["text"])) {
 	$file_id = '';
 	$cmd_user = $eqLogic->getCmd('action', $json["message"]["chat"]["id"]);
 	if (is_object($cmd_user)) {
-        $geolocCmd = geotravCmd::byEqLogicIdAndLogicalId(str_replace('#', '', str_replace('eqLogic', '', $cmd_user->getConfiguration('cmdgeoloc', '')),'location:updateCoo');
+        $geolocCmd = geotravCmd::byEqLogicIdAndLogicalId(str_replace('#', '', str_replace('eqLogic', '', $cmd_user->getConfiguration('cmdgeoloc', ''))),'location:updateCoo');
         $option = array('message' => , $json["message"]["location"]["latitude"] . ',' . $json["message"]["location"]["longitude"]);
 		$geolocCmd->execute($option);
 	}
